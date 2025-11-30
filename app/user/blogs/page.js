d78cf520e68f37410a962f44526d957951022ebf
alinawaz <ali0578600@gmail.com>
2025-11-30 16:25:40 +0500

    updloaded

--- app/user/blogs/page.js
+++ app/user/blogs/page.js
@@ -56,7 +56,7 @@ const page = () => {
         <Sidebar />
         <div className='w-full border mx-5 md:mx-0 h-[80vh] overflow-scroll border-gray-300 md:w-[70vw]' >
           <div className='my-4 ' >
-      <Link href={"/user/addblog"} className='text-white px-3 py-1 rounded-md  bg-[#7e39f2] mx-2 roundedmd' >Add blog</Link>
+            <Link href={"/user/addblog"} className='text-white px-3 py-1 rounded-md  bg-[#7e39f2] mx-2 roundedmd' >Add blog</Link>
           </div>
           <div className='grid font-medium bg-gray-300 py-3 text-center px-2 grid-cols-5' >
             <p>Author</p>
@@ -73,11 +73,9 @@ const page = () => {
                 <p>{item.category}</p>
                 <p>2025</p>
                 <div className='flex gap-3 justify-center'>
-                  <div onClick={() => {
-                    router.push(`/user/edit/${item._id}`)
-                  }} >
+                  <Link href={`/user/edit/${item._id}`} >
                     <svg className='w-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M15.7279 9.57627L14.3137 8.16206L5 17.4758V18.89H6.41421L15.7279 9.57627ZM17.1421 8.16206L18.5563 6.74785L17.1421 5.33363L15.7279 6.74785L17.1421 8.16206ZM7.24264 20.89H3V16.6473L16.435 3.21231C16.8256 2.82179 17.4587 2.82179 17.8492 3.21231L20.6777 6.04074C21.0682 6.43126 21.0682 7.06443 20.6777 7.45495L7.24264 20.89Z"></path></svg>
-                  </div>
+                  </Link>
                   <div onClick={() => deletedBlog(item._id)} >
                     <svg className='w-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M7 4V2H17V4H22V6H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V6H2V4H7ZM6 6V20H18V6H6ZM9 9H11V17H9V9ZM13 9H15V17H13V9Z"></path></svg>
                   </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
                             <svg className='w-6 top-5 right-2   absolute ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"></path></svg>
                         </div>
                         : null}
-                </div> : <button onClick={() => router.push("/login")} className='px-5 py-1 rounded-full text-white bg-[#7e39f2] ' >Log In</button>}
+                </div> : <Link href={'/login'} className='px-5 py-1 rounded-full text-white bg-[#7e39f2] ' >Log In</Link>}
             </div>
         </div>
     )
